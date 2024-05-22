94d455e81d2f7dcdef37436a09eb9d7f62b2b7f9
Bilal Ozdemir <ozdemirbilal01@gmail.com>
2024-05-22 11:11:24 +0300

    css added

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
+import './index.css';
 
 export function App() {
-    return <>React App</>;
+    return <>
+    React App
+    //INLINE STYLE
+    <div style = {{color: 'red'}}>INLINE STYLE</div>
+    //EXTERNAL STYLE İŞLEMİ
+    <div className="container">External CSS</div>
+    </>;
+    
 }
 
 const rootElement = ReactDOM.createRoot(
